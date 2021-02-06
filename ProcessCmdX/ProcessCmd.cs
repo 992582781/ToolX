05035ddb4228f6d0e0f7e42fb92c66645a695482
992582781 <992582781@qq.com>
2021-02-06 10:59:39 +0800

    C# 调用cmd.exe命令行命令 net use

---
+++ ProcessCmdX/ProcessCmd.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessCmdX
+{
+    public class ProcessCmd
+    {
+        public static bool Connect(string Path, string UserName, string PassWord)
+        {
+            bool flag = false;
+            Process process = new Process();
+            try
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+                string dosLine = @"net use " + "" + Path + " " + "/user:" + UserName + " " + PassWord;
+                // MessageBox.Show(dosLine);
+                process.StandardInput.WriteLine(dosLine);
+                process.StandardInput.WriteLine("exit");
+                while (!process.HasExited)
+                {
+                    process.WaitForExit(1000);
+                }
+                string errormsg = process.StandardError.ReadToEnd();
+                process.StandardError.Close();
+                if (string.IsNullOrEmpty(errormsg))
+                {
+                    flag = true;
+                }
+                else
+                {
+                    throw new Exception(errormsg);
+
+                }
+            ;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                process.Close();
+                process.Dispose();
+            }
+
+            return flag;
+        }
+    }
+}
